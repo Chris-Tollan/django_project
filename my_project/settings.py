3d1a3069f0f91e805baef619eae406f3f59e563d
Chris-Tollan <>
2023-12-15 20:35:51 +0000

    amend debug to false

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b45@htbl!j34pt(0+fnove2-1x^wf$exn-%w+m)$rdjnq9&(i='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-chris-tollan-django-proj-s7y69w68tw.us2.codeanyapp.com', 
                  '.herokuapp.com']
